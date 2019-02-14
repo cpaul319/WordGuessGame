fbdd11f88dd122a5d05411e870b854889d362bf2
Chris Paul <34114230+cpaul319@users.noreply.github.com>
2019-02-14 13:21:19 -0500

    manual update 2/14/19

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,72 @@
 
 // variables
-var guess = " ";
+var guess = " x";
 var guessedWordArr = [];
 var incorrectGuessedArr = [];
 var wordSpace = [];
 // var wordGuessCount = 0;
- 
+
 // var guessedLetter = "";
 var remainingGuesses = 10;
 var wins = 0;
 var losses = 0;
 
 //object declarations
+function getRandomWord() {
+   var jesseJames = {
+      "firstname": ["J", "E", "S", "S", "E"],
+      "lastname": "James"
+   };
 
-var jesseJames = {
-   "firstname": ["J", "E", "S", "S", "E"],
-   "lastname": "James"
-};
+   var wildBillHitchcock = {
 
-var wildBillHitchcock = {
+      "firstname": ["W", "I", "L", "D", "B", "I", "L", "L"],
+      "lastname": "Hitchcock"
+   };
 
-   "firstname": ["W", "I", "L", "D", "B", "I", "L", "L"],
-   "lastname": "Hitchcock"
-};
+   var annieOakley = {
+      "firstname": ["A", "N", "N", "I", "E"],
+      "lastname": "Oakley"
 
-var annieOakley = {
-   "firstname": ["A", "N", "N", "I", "E"],
-   "lastname": "Oakley"
+   };
 
-};
+   var billyTheKid = {
+      "firstname": ["B", "I", "L", "L", "Y"],
+      "lastname": "The Kid"
+   };
 
-var billyTheKid = {
-   "firstname": ["B", "I", "L", "L", "Y"],
-   "lastname": "The Kid"
-};
+   var buffaloBill = {
+      "firstname": ["B", "U", "F", "F", "A", "L", "O"],
+      "lastname": "Bill"
+   };
 
-var buffaloBill = {
-   "firstname": ["B", "U", "F", "F", "A", "L", "O"],
-   "lastname": "Bill"
-};
+   var butchCassidy = {
+      "firstname": ["B", "U", "T", "C", "H"],
+      "lastname": "Cassidy"
+   };
 
-var butchCassidy = {
-   "firstname": ["B", "U", "T", "C", "H"],
-   "lastname": "Cassidy"
-};
+   var docHoliday = {
+      "firstname": ["D", "O", "C"],
+      "lastname": "Holiday"
+   };
+   var words = [jesseJames, wildBillHitchcock, annieOakley, billyTheKid, buffaloBill, butchCassidy, docHoliday];
+   var randomWord = Math.floor(Math.random() * words.length);
+   var chosenWord = words[randomWord];
+   return chosenWord;
+}
+function wins() {
+   wins++;
+   resetGame();
+}
+function reset() {
 
-var docHoliday = {
-   "firstname": ["D", "O", "C"],
-   "lastname": "Holiday"
-};
-var words = [jesseJames, wildBillHitchcock, annieOakley, billyTheKid, buffaloBill, butchCassidy, docHoliday];
-var randomWord = Math.floor(Math.random() * words.length);
-var chosenWord = words[randomWord];
- 
+   guessWordArr = [];
+   document.getElementById("userLives").innerHTML = guessLetterCount;
+   document.getElementById("losses").innerHTML = losses;
+   document.getElementById("wordGuess").innerHTML = guessLetterArr;
+   randomLetter = getRandomLetter();
+}
+
 // document.onkeyup = function(event) {
 //    userText.textContent = event.key;
 //  };
@@ -77,72 +91,107 @@ $(document).ready(function () {
       letterClick.text($(this).attr("data-letter"));
 
       $("#display").append(letterClick);
-      showLetter();
+      showLetter(guess);
+
+
+      function showLetter() {
+         return guess;
+      }
+
+
+      wordGuessLoop();
+      
    });
-   function showLetter(letter) {
-		 	   
-   }
-  
-   function myFunction() {
-      fruits.splice(2, 0, "Lemon", "Kiwi");
-      document.getElementById("demo").innerHTML = fruits;
-    }
-   // showLetter();
-
+   function wordGuessLoop() {
+      wordGuess();
+      showLetter(guess);
       for (var j = 0; j < chosenWord.firstname.length; j++) {
-
-
-         if (chosenWord.firstname[j] === guess) {
-            guessedWordArr.push(guess);
-            
-            wordSpace[j] = "_";
-         wordSpace.splice([j],0, guessedWordArr[j])
-         document.getElementById("wordGuess").innerHTML= wordSpace;
-            console.log(wordSpace);
-          
-         }
-         
+         // (chosenWord.firstname[j] !== guess) {
+         guessedWordArr.push(guess);
          wordSpace[j] = "_";
-         // wordSpace.splice([j],1, guessedWordArr[j])
-         document.getElementById("wordGuess").innerHTML= wordSpace;
-            console.log(wordSpace);
-          //remaining guesses counter
-      if ((chosenWord.firstname[j] === guess) || (guessedWordArr[j] === guess)){
-         remainingGuesses--;
-         console.log(remainingGuesses);
-      }
-         
-      }
+         wordSpace.splice([j], 0, guessedWordArr[j]);
+         document.getElementById("wordGuess").innerHTML = wordSpace;
+         console.log(guess);
+         console.log(chosenWord.firstname);
+         console.log(wordSpace);
+         outPuts();
 
-      //letters guessed output to user
-      if (chosenWord.firstname[j] !== guess); {
-
-         incorrectGuessedArr[j] = guess;
-         console.log(incorrectGuessedArr);
-        
-         // incorrectGuessedArr.push(guessedWordArr);
-         document.getElementById("display").innerHTML = incorrectGuessedArr;
       }
-      // wins, losses counter, end of game
-      if (chosenWord.firstname === guessedWordArr){
-   
-         alert("You win");
-     wins ++;
-         }
-         if (remainingGuesses < 1){
-            alert("You lost");
-            losses++;
-         }
+   }
+
+
+   function wordGuess() {
+      showLetter();
+      getRandomWord();
+      guessedWordArr = [];
+      wordSpace = [];
+      if (chosenWord.firstname === guess) {
+         endGame();
+      }
+      else {
+         gameLoop();
+         outputs();
+
+      }
+   }
+
+
+   function outPuts() {
+      document.getElementById("userlives").innerHTML = remainingGuesses;
+      document.getElementById("gameInstructions").innerHTML = "Click letter to begin game";
+      document.getElementById("wins").innerHTML = wins;
+      document.getElementById("losses").innerHTML = losses;
+      document.getElementById("lastname").innerHTML = chosenWord.lastname;
+
+   }
+
+
+});
+   // function myFunction() {
+   //    fruits.splice(2, 0, "Lemon", "Kiwi");
+   //    document.getElementById("demo").innerHTML = fruits;
+   //  }
+   // // showLetter();
+
+
+
+
+   //       wordSpace[j] = "_";
+   //       // wordSpace.splice([j],1, guessedWordArr[j])
+   //       document.getElementById("wordGuess").innerHTML= wordSpace;
+   //          console.log(wordSpace);
+   //        //remaining guesses counter
+   //    if ((chosenWord.firstname[j] === guess) || (guessedWordArr[j] === guess)){
+   //       remainingGuesses--;
+   //       console.log(remainingGuesses);
+   //    }
+
+   //    }
+
+   //    //letters guessed output to user
+   //    if (chosenWord.firstname[j] !== guess); {
+
+   //       incorrectGuessedArr[j] = guess;
+   //       console.log(incorrectGuessedArr);
+
+   //       // incorrectGuessedArr.push(guessedWordArr);
+   //       document.getElementById("display").innerHTML = incorrectGuessedArr;
+   //    }
+   //    // wins, losses counter, end of game
+   //    if (chosenWord.firstname === guessedWordArr){
+
+   //       alert("You win");
+   //   wins ++;
+   //       }
+   //       if (remainingGuesses < 1){
+   //          alert("You lost");
+   //          losses++;
+   //       }
 //outputs to user
-   document.getElementById("userlives").innerHTML = remainingGuesses;
-   document.getElementById("gameInstructions").innerHTML = "Click letter to begin game";
-   document.getElementById("wins").innerHTML = wins;
-   document.getElementById("losses").innerHTML = losses;
-   document.getElementById("lastname").innerHTML = chosenWord.lastname;
 
-   
 
-}); 
+
+
 
 
 
@@ -178,19 +227,19 @@ $(document).ready(function () {
 
    //    while (lettersLeft > 0) {
    //       alert(guessedWordArr.join(" "));
-         
-
-      
-
-   
 
 
 
 
-   
 
-   
- 
+
+
+
+
+
+
+
+
 
 
 
